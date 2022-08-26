6d9cf30a6d8025043eb03b34b20e09638c78e852
pedro pablo piñeros guerrero <pedropablopinerosg@hotmail.com>
2022-08-26 08:03:29 -0500

    se trae token para inicio de sesion

--- clientes-app/src/app/usuarios/login.component.ts
+++ clientes-app/src/app/usuarios/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Usuario} from './usuario';
+import Swal from "sweetalert2";
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  titulo:string = "Por favor Sign In!"
+  titulo:string = "Por favor Sign In!";
+  usuario:Usuario;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router : Router
+  ) {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit(): void {
   }
 
+  login():void{
+
+    console.log(this.usuario)
+    if(this.usuario.username == null || this.usuario.password == null){
+      Swal.fire('Error Login','Username o password estan vacias!', 'error');
+      return;
+    }
+    this.authService.login(this.usuario).subscribe(response =>{
+      console.log(response);
+      this.router.navigate(['/clientes']);
+      Swal.fire('Login', `Hola ${response.username}, inicio con extio sesión`, 'success')
+    })
+  }
 }
